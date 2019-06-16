c4e416867d9ec2bf228ff08402c955be37baf9f2
Anastasia <anastasy45@ya.ru>
2019-06-16 14:04:07 +0500

    parts of coefStandarts was added

--- src/components/coefStandarts/index.js
+++ src/components/coefStandarts/index.js
@@ -1,7 +1,11 @@
 import * as R from 'ramda'
 import coefPart_1_20 from './coefPart_1_20'
+import coefPart_21_40 from './coefPart_21_40'
+import coefPart_41_60 from './coefPart_41_60'
+import coefPart_61_80 from './coefPart_61_80'
+import coefPart_81_100 from './coefPart_81_100'
 
-const ElectricReceiverArr = [...coefPart_1_20]
+const ElectricReceiverArr = [...coefPart_1_20, ...coefPart_21_40, ...coefPart_41_60, ...coefPart_61_80, ...coefPart_81_100]
 
 const createObj = (el, i) => ({
 	count: i + 1,
